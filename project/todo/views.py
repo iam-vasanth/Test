b9f1cd0d15ac8193c6ccf9d8e7363ca5f9630300
raja9112 <rrn.rajarajan.gmail.com>
2023-11-02 14:08:12 +0530

    Added templates and statisc files and changed settings.py file, Created models, configured admin.py and views.py and urls.py of app and project

--- project/todo/views.py
+++ project/todo/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def home(request):
+    return render(request, 'homepage.html')
+
+def login(request):
+    return render(request, 'login.html')
+
+def register(request):
+    return render(request, 'register.html')
