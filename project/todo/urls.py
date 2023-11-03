05d6fb215dc00fabaab1e8b3ef1f526397dc587e
raja9112 <rrn.rajarajan.gmail.com>
2023-11-03 12:35:09 +0530

    Moved notification bar code to base.html and Class based login process code done and logout code done. No error

--- project/todo/urls.py
+++ project/todo/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from . import views
-from .views import home, login, SignUp
+from .views import home, login, SignUp, SignIn
 
 urlpatterns=[
     path('', views.home, name='Home'),
-#     path('login/', views.login, name='Login'),
+    path('login/', SignIn.as_view(), name='Login'),
     path('register/', SignUp.as_view(), name='Register'),
+    path('logout/', views.logout, name='Logout'),
+
     
 ]
--- project/todo/views.py
+++ project/todo/views.py
@@ -14,22 +14,51 @@ class SignUp(View):
         if password == confirm_password:
             if User.objects.filter(username=username).exists():
                 messages.info(request, "Username already taken")
-                return redirect('register')
+                return redirect('/register')
             else:
                 creating_user= User.objects.create_user(username=username,
                                                         password=confirm_password)
                 
                 creating_user.save()
                 auth.login(request, creating_user)
-                messages.info(request, "Welcome to ToDO App")
+                messages.info(request, "Welcome to ToDo App")
                 return redirect('/')
 
         else:
             messages.info(request, "Password did not match")
-            return redirect('register')
+            return redirect('/register')
 
     def get(self, request):
         return render(request, 'register.html')
+
+
+
+
+# Code for Sign in
+class SignIn(View):
+    def post(self, request):
+        username = request.POST.get('username_login')
+        password = request.POST.get('password_login')
+
+        user_login = auth.authenticate(username=username, password=password)
+
+        if user_login is not None:
+            auth.login(request, user_login)
+            messages.info(request, 'Welcome back!')
+            return redirect('/')
+            
+        else:
+            messages.info(request, 'Username or password is incorrect')
+            return redirect('/login')
+
+    def get(self, request):
+        return render(request, 'login.html')
+
+
+def logout(request):
+    auth.logout(request)
+    return redirect('/')
+
 
 
 def home(request):
