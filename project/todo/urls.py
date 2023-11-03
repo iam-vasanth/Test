b09362a97ecda86785afb5395244b713dad1b27b
raja9112 <rrn.rajarajan.gmail.com>
2023-11-03 11:43:44 +0530

    Class based views is added for SignUp process/ No Error

--- project/todo/urls.py
+++ project/todo/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
+from .views import home, login, SignUp
 
 urlpatterns=[
     path('', views.home, name='Home'),
-    path('login/', views.login, name='Login'),
-    path('register/', views.register, name='Register'),
+#     path('login/', views.login, name='Login'),
+    path('register/', SignUp.as_view(), name='Register'),
     
 ]
--- project/todo/views.py
+++ project/todo/views.py
@@ -1,6 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User
+from django.contrib import auth
+from django.views import View
+from django.contrib import messages
 
-# Create your views here.
+#Code for Account Sing up
+class SignUp(View):
+    def post(self,request):
+        username = request.POST.get('username_r')
+        password = request.POST.get('password_r')
+        confirm_password = request.POST.get('password_rc')
+
+        if password == confirm_password:
+            if User.objects.filter(username=username).exists():
+                messages.info(request, "Username already taken")
+                return redirect('register')
+            else:
+                creating_user= User.objects.create_user(username=username,
+                                                        password=confirm_password)
+                
+                creating_user.save()
+                messages.info(request, "Welcome to ToDO App")
+                return redirect('/')
+
+        else:
+            messages.info(request, "Password did not match")
+            return redirect('register')
+
+    def get(self, request):
+        return render(request, 'register.html')
+
 
 def home(request):
     return render(request, 'homepage.html')
