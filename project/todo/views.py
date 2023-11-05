8fb85e9d6292adc59d8582ab4226281a2212ac88
raja9112 <rrn.rajarajan@gmail.com>
2023-11-05 05:38:35 +0530

    Name attribute was not given in HTML file, and confifured some styling error denoted ny pylint and flake8

--- project/todo/views.py
+++ project/todo/views.py
@@ -1,17 +1,16 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.models import User
-from django.contrib import auth
+from django.contrib import auth, messages
 from django.views import View
-from django.contrib import messages
 from todo.models import Task
 # from django.contrib.auth.decorators import login_required
-from django.contrib.auth.mixins import LoginRequiredMixin
 # from django.http import HttpResponseRedirect
 
 
-#Code for Account Sing up
+# Code for Account Sing up
 class SignUp(View):
-    def post(self,request):
+    def post(self, request):
         username = request.POST.get('username_r')
         password = request.POST.get('password_r')
         confirm_password = request.POST.get('password_rc')
@@ -20,23 +19,17 @@ class SignUp(View):
             if User.objects.filter(username=username).exists():
                 messages.info(request, "Username already taken")
                 return redirect('/register')
-            else:
-                creating_user= User.objects.create_user(username=username,
-                                                        password=confirm_password)
-                
-                creating_user.save()
-                auth.login(request, creating_user)
-                messages.info(request, "Welcome to ToDo App")
-                return redirect('/')
+            creating_user = User.objects.create_user(username = username, password = confirm_password)
+            creating_user.save()
+            auth.login(request, creating_user)
+            messages.info(request, "Welcome to ToDo App")
+            return redirect('/')
 
-        else:
-            messages.info(request, "Password did not match")
-            return redirect('/register')
+        messages.info(request, "Password did not match")
+        return redirect('/register')
 
     def get(self, request):
         return render(request, 'register.html')
-
-
 
 
 # Code for Sign in
@@ -51,10 +44,8 @@ class SignIn(View):
             auth.login(request, user_login)
             messages.info(request, 'Welcome back!')
             return redirect('/')
-            
-        else:
-            messages.info(request, 'Username or password is incorrect')
-            return redirect('/login')
+        messages.info(request, 'Username or password is incorrect')
+        return redirect('/login')
 
     def get(self, request):
         return render(request, 'login.html')
@@ -65,7 +56,7 @@ def logout(request):
     return redirect('/')
 
 
-# Code for Homepage 
+# Code for Homepage
 class HomePage(LoginRequiredMixin, View):  # Inherit from LoginRequiredMixin
     login_url = '/login/'  # Set the login URL
 
@@ -88,9 +79,8 @@ class HomePage(LoginRequiredMixin, View):  # Inherit from LoginRequiredMixin
 
         except Exception as e:
             print(e)
-            messages.error(request, 'Error creating task: {}'.format(e))
+            messages.error(request, f'Error creating task: {format(e)}')
             return redirect('/')
-
 
     def get(self, request):
         return render(request, 'homepage.html')
@@ -101,6 +91,7 @@ class HomePage(LoginRequiredMixin, View):  # Inherit from LoginRequiredMixin
 
 def login(request):
     return render(request, 'login.html')
+
 
 def register(request):
     return render(request, 'register.html')
